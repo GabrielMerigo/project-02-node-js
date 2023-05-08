9dcc23a1005fe48c194c8a1ca49a4621f0422154
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-08 05:49:09 -0300

    adding cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -26,6 +26,14 @@ export const transactionsRoutes = async (app: FastifyInstance) => {
     };
   });
 
+  app.get("/summary", async () => {
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .first();
+
+    return { summary };
+  });
+
   app.post("/", async (req, res) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -35,10 +43,21 @@ export const transactionsRoutes = async (app: FastifyInstance) => {
 
     const { amount, title, type } = createTransactionBodySchema.parse(req.body);
 
+    let sesssionId = req.cookies.sessionId;
+
+    if (!sesssionId) {
+      sesssionId = crypto.randomUUID();
+      res.cookie("sesssionId", sesssionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     await knex("transactions").insert({
       id: crypto.randomUUID(),
       title,
       amount: type === "credit" ? amount : amount * -1,
+      session_id: sesssionId,
     });
 
     return res.status(201).send();
